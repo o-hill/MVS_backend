4558dd91f11c34e937833b8c47da1e78a2b201e9
Oliver <oliverhi@umich.edu>
2019-01-07 07:35:38 -0700

    Working on processing options.

--- database.py
+++ database.py
@@ -14,11 +14,16 @@ client = MongoClient()
 
 CELL_TYPES = ['WBC', 'RBC', 'Stem Cell']
 
-PROCESSING_OPTIONS = {
-    'Cell Segmentation': { 'Cell Type': ('LIST', CELL_TYPES), 'Distance': ('DOUBLE') },
-    'Cell Count': { 'Cell Type': ('LIST', CELL_TYPES) }
-    'Clean': { 'Uniform Elimination': ('BOOLEAN') }
-}
+PROCESSING_OPTIONS = [
+    { 'name': 'Cell Segmentation', options: [
+        { 'name': 'Cell Type', fields: ('LIST', CELL_TYPES) },
+        { 'name': 'Distance', fields: ('DOUBLE') }
+    ]},
+    { 'name: 'Cell Count', options: [
+        { name: 'Cell Type', fields: ('LIST', CELL_TYPES) }
+    ]},
+    { name: 'Clean', options: [{ name: 'Uniform Elimination', fields: ('BOOLEAN') }]}
+]
 
 
 class MongoDatabase:
@@ -27,25 +32,27 @@ class MongoDatabase:
         '''Initialize access to the database.'''
 
         self.db = client.MVS
-        self.videos_collection = self.db.videos
+        self.videos = self.db.videos
 
 
     def list_videos(self):
         '''Return a list of filenames for available videos.'''
-        return [ x['filename'] for x in self.videos_collection.find() ]
+        videos = list(self.videos.find({}, {'_id': 0}))
+        return sorted(videos, key=lambda x: x['filename'])
 
 
     def add_video(self, filename, save_dir):
         '''Add a video filename to the database.'''
 
-        self.videos_collection.insert_one({
+        self.videos.insert_one({
             'filename': filename,
-            'save_directory': save_dir
+            'save_directory': save_dir,
+            'processing_options': PROCESSING_OPTIONS
         })
 
 
     def delete_video(self, filename):
         '''Delete a video from the database.'''
-        self.videos_collection.delete_one({ 'filename': filename })
+        self.videos.delete_one({ 'filename': filename })
 
 
